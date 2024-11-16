52c7a3292452433c643e42d1b459b0a3bf189dc7
Petr Horacek <petr@zlosynth.com>
2024-11-16 02:44:05 +0100

    Allow configuration of target artifact dir
    Signed-off-by: Petr Horacek <petr@zlosynth.com>

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -3,7 +3,8 @@
 set -xe
 
 SOURCE_DIR=$(dirname $0)
-TARGET=$1
+TARGET=${1:-/tmp}
 
-${SOURCE_DIR}/gis/fetch_and_process.sh /tmp/test.jpg
-ditherer /tmp/test.jpg /tmp/test-dithered.png /tmp/test-dithered.bitmap
+${SOURCE_DIR}/gis/fetch_and_process.sh ${TARGET}/view.jpg
+ditherer ${TARGET}/view.jpg ${TARGET}/view-dithered.png ${TARGET}/view-dithered.bitmap
+date +%s > ${TARGET}/timestamp.txt
